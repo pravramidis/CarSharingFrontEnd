85ea1e3e5f28099ec694562b8906a2303286393a
ctsirka <xarat19@gmail.com>
2023-12-29 16:24:39 +0200

    Add profile functionality

--- app/src/main/java/com/CharaProdromos/carsharing/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/CharaProdromos/carsharing/ui/profile/ProfileFragment.java
@@ -2,6 +2,7 @@ package com.CharaProdromos.carsharing.ui.profile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,8 +33,13 @@ public class ProfileFragment extends Fragment {
     ImageView profilePic;
     EditText username;
     EditText password;
+    EditText name;
+    EditText license;
+    EditText birthday;
+    EditText phoneNumber;
+    EditText email;
     MaterialButton updateButton;
-    ProgressBar progressBar;
+
 
 
 
@@ -49,17 +55,108 @@ public class ProfileFragment extends Fragment {
         profilePic = root.findViewById(R.id.imageView);
         username = root.findViewById(R.id.edit_username);
         password = root.findViewById(R.id.edit_password);
+        name = root.findViewById(R.id.edit_name);
+        license = root.findViewById(R.id.edit_licence);
+        birthday = root.findViewById(R.id.edit_dob);
+        phoneNumber = root.findViewById(R.id.edit_phone_number);
+        email = root.findViewById(R.id.edit_email);
         updateButton = root.findViewById(R.id.buttonUpdate);
 
         String user = GlobalVariables.getInstance().getUsername();
         httpRequestGetUserInfo(user);
         username.setText(user);
 
+        updateButton.setOnClickListener((v -> {
+            updateButtonClick();
+
+        }));
+
 
 
 
         return root;
     }
+
+    private void updateButtonClick() {
+        String newUsername = username.getText().toString();
+        String newPassword = password.getText().toString();
+        String newName = name.getText().toString();
+        String newDate = birthday.getText().toString();
+        String newLicense = license.getText().toString();
+        String newPhoneNumber = phoneNumber.getText().toString();
+        String newEmail = email.getText().toString();
+
+        if (newUsername.isEmpty() || newPassword.isEmpty() || newName.isEmpty() || newDate.isEmpty() || newLicense.isEmpty() || newPhoneNumber.isEmpty()) {
+            Toast toast = Toast.makeText(getActivity().getApplicationContext(), "Please fill all fields", Toast.LENGTH_LONG);
+            toast.show();
+            return;
+        }
+
+        httpRequestUpdateInfo(newUsername, newPassword, newName, newDate, newLicense, newPhoneNumber, newEmail);
+
+    }
+
+    private void httpRequestUpdateInfo(String newUsername, String newPassword, String newName, String newDate, String newLicense, String newPhoneNumber, String newEmail) {
+        JSONObject jsonBody = new JSONObject();
+        String user = GlobalVariables.getInstance().getUsername();
+        try {
+            jsonBody.put("oldUsername", user);
+            jsonBody.put("Username", newUsername);
+            jsonBody.put("Password", newPassword);
+            jsonBody.put("name", newName);
+            jsonBody.put("date", newDate);
+            jsonBody.put("licenseId", newLicense);
+            jsonBody.put("phoneNumber", newPhoneNumber);
+            jsonBody.put("email", newEmail);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String serverAddress = this.getString(R.string.serverAddress);
+        System.out.println(jsonBody.toString());
+        String url = serverAddress + "/users/update";
+        System.out.println(url);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+                            if (response.getString("message").equals("User already exists")) {
+                                String text = "Username already exists";
+                                Toast toast = Toast.makeText(getActivity().getApplicationContext(), text, Toast.LENGTH_LONG);
+                                System.out.println("Success: " + response.toString());
+                                toast.show();
+
+                                username.setText(user);
+                            }
+                            else {
+                                GlobalVariables.getInstance().setUsername(newUsername);
+                            }
+                        }
+                        catch (Exception exception) {
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle errors
+                        // You can log the error or show a message to the user
+
+                        String text = "Fail"+ error.toString();
+                        Toast toast = Toast.makeText(getActivity().getApplicationContext(),text, Toast.LENGTH_LONG);
+                        System.out.println("Fail"+ error.toString());
+                        toast.show();
+                    }
+                });
+
+
+        // Add the request to the RequestQueue
+        Volley.newRequestQueue(getActivity().getApplicationContext()).add(jsonObjectRequest);
+
+    }
+
 
     private void httpRequestGetUserInfo(String username) {
         JSONObject jsonBody = new JSONObject();
@@ -87,7 +184,15 @@ public class ProfileFragment extends Fragment {
                             String currBirthday = response.getString("Date_of_birth");
                             String currPassword = response.getString("Password");
 
+                            name.setText(currName);
+                            email.setText(currEmail);
+                            license.setText(currId);
+                            phoneNumber.setText(currPhoneNum);
+                            birthday.setText(currBirthday);
+                            password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
                             password.setText(currPassword);
+
+
 
 
 
