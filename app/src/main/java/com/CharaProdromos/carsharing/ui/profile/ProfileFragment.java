3b114cf42b2e8cd60e21a6e28023a069b6204552
ctsirka <xarat19@gmail.com>
2024-01-11 12:42:48 +0200

    Add Delete Account

--- app/src/main/java/com/CharaProdromos/carsharing/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/CharaProdromos/carsharing/ui/profile/ProfileFragment.java
@@ -13,12 +13,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.content.Context;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import com.CharaProdromos.carsharing.GlobalVariables;
 import com.CharaProdromos.carsharing.History;
+import com.CharaProdromos.carsharing.MainActivity;
 import com.CharaProdromos.carsharing.R;
 import com.CharaProdromos.carsharing.UserLogin;
 import com.CharaProdromos.carsharing.UserRegistration;
@@ -56,6 +58,10 @@ public class ProfileFragment extends Fragment {
         MaterialButton history = root.findViewById(R.id.buttonHistory);
         MaterialButton edit = root.findViewById(R.id.buttonEdit);
         MaterialButton logout = root.findViewById(R.id.buttonLogout);
+        MaterialButton delete = root.findViewById(R.id.buttonDelete);
+        CardView deleteMsg = root.findViewById(R.id.card_view);
+        MaterialButton yes = root.findViewById(R.id.buttonYes);
+        MaterialButton no = root.findViewById(R.id.buttonNo);
 
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,221 +95,83 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                delete.setVisibility(View.GONE);
+                deleteMsg.setVisibility(View.VISIBLE);
+
+
+            }
+        });
+
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteMsg.setVisibility(View.GONE);
+                delete.setVisibility(View.VISIBLE);
+            }
+        });
+
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteMsg.setVisibility(View.GONE);
+                httpRequestDeleteAcc();
+
+            }
+
+        });
+
 
 
         return root;
     }
+
+    private void httpRequestDeleteAcc() {
+        JSONObject jsonBody = new JSONObject();
+        String user = GlobalVariables.getInstance().getUsername();
+
+        try {
+            jsonBody.put("Username",user);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        String serverAddress = this.getString(R.string.serverAddress);
+        System.out.println(jsonBody.toString());
+        String url = serverAddress + "/users/delete";
+        System.out.println(url);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println("Deleted user account");
+                        try {
+                            if (response.getString("message").equals("User deleted successfully.")) {
+                                String text = "Deleted successfully";
+                                Toast toast = Toast.makeText(getActivity().getApplicationContext(), text, Toast.LENGTH_LONG);
+                                toast.show();
+                                Intent intent = new Intent(getActivity(), UserLogin.class);
+                                startActivity(intent);
+                            }
+                        }
+                        catch (Exception ex){
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        String text = "Fail"+ error.toString();
+                    }
+                });
+        Volley.newRequestQueue(getActivity().getApplicationContext()).add(jsonObjectRequest);
+
+
+    }
 }
 
 
-
-//    ImageView profilePic;
-//    EditText username;
-//    EditText password;
-//    EditText name;
-//    EditText license;
-//    EditText birthday;
-//    EditText phoneNumber;
-//    EditText email;
-//    MaterialButton updateButton;
-//
-//
-//
-//
-//    private FragmentProfileBinding binding;
-//
-//    public View onCreateView(@NonNull LayoutInflater inflater,
-//                             ViewGroup container, Bundle savedInstanceState) {
-//
-//
-//        binding = FragmentProfileBinding.inflate(inflater, container, false);
-//        View root = binding.getRoot();
-//
-//        profilePic = root.findViewById(R.id.imageView);
-//        username = root.findViewById(R.id.edit_username);
-//        password = root.findViewById(R.id.edit_password);
-//        name = root.findViewById(R.id.edit_name);
-//        license = root.findViewById(R.id.edit_licence);
-//        birthday = root.findViewById(R.id.edit_dob);
-//        phoneNumber = root.findViewById(R.id.edit_phone_number);
-//        email = root.findViewById(R.id.edit_email);
-//        updateButton = root.findViewById(R.id.buttonUpdate);
-//
-//        String user = GlobalVariables.getInstance().getUsername();
-//        httpRequestGetUserInfo(user);
-//        username.setText(user);
-//
-//        updateButton.setOnClickListener((v -> {
-//            updateButtonClick();
-//
-//        }));
-//
-//
-//
-//
-//        return root;
-//    }
-//
-//    private void updateButtonClick() {
-//        String newUsername = username.getText().toString();
-//        String newPassword = password.getText().toString();
-//        String newName = name.getText().toString();
-//        String newDate = birthday.getText().toString();
-//        String newLicense = license.getText().toString();
-//        String newPhoneNumber = phoneNumber.getText().toString();
-//        String newEmail = email.getText().toString();
-//
-//        if (newUsername.isEmpty() || newPassword.isEmpty() || newName.isEmpty() || newDate.isEmpty() || newLicense.isEmpty() || newPhoneNumber.isEmpty()) {
-//            Toast toast = Toast.makeText(getActivity().getApplicationContext(), "Please fill all fields", Toast.LENGTH_LONG);
-//            toast.show();
-//            return;
-//        }
-//
-//        httpRequestUpdateInfo(newUsername, newPassword, newName, newDate, newLicense, newPhoneNumber, newEmail);
-//
-//    }
-//
-//    private void httpRequestUpdateInfo(String newUsername, String newPassword, String newName, String newDate, String newLicense, String newPhoneNumber, String newEmail) {
-//        JSONObject jsonBody = new JSONObject();
-//        String user = GlobalVariables.getInstance().getUsername();
-//        try {
-//            jsonBody.put("oldUsername", user);
-//            jsonBody.put("Username", newUsername);
-//            jsonBody.put("Password", newPassword);
-//            jsonBody.put("name", newName);
-//            jsonBody.put("date", newDate);
-//            jsonBody.put("licenseId", newLicense);
-//            jsonBody.put("phoneNumber", newPhoneNumber);
-//            jsonBody.put("email", newEmail);
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-//        String serverAddress = this.getString(R.string.serverAddress);
-//        System.out.println(jsonBody.toString());
-//        String url = serverAddress + "/users/update";
-//        System.out.println(url);
-//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonBody,
-//                new Response.Listener<JSONObject>() {
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//
-//                        try {
-//                            if (response.getString("message").equals("User already exists")) {
-//                                String text = "Username already exists";
-//                                Toast toast = Toast.makeText(getActivity().getApplicationContext(), text, Toast.LENGTH_LONG);
-//                                System.out.println("Success: " + response.toString());
-//                                toast.show();
-//
-//                                username.setText(user);
-//                            }
-//                            else {
-//                                GlobalVariables.getInstance().setUsername(newUsername);
-//                                username.setText(newUsername);
-//                                name.setText(newName);
-//                                email.setText(newEmail);
-//                                license.setText(newLicense);
-//                                phoneNumber.setText(newPhoneNumber);
-//                                birthday.setText(newDate);
-//                                password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-//                                password.setText(newPassword);
-//                            }
-//                        }
-//                        catch (Exception exception) {
-//
-//                        }
-//                    }
-//                },
-//                new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        // Handle errors
-//                        // You can log the error or show a message to the user
-//
-//                        String text = "Fail"+ error.toString();
-//                        Toast toast = Toast.makeText(getActivity().getApplicationContext(),text, Toast.LENGTH_LONG);
-//                        System.out.println("Fail"+ error.toString());
-//                        toast.show();
-//                    }
-//                });
-//
-//
-//        // Add the request to the RequestQueue
-//        Volley.newRequestQueue(getActivity().getApplicationContext()).add(jsonObjectRequest);
-//
-//    }
-//
-//
-//    private void httpRequestGetUserInfo(String username) {
-//        JSONObject jsonBody = new JSONObject();
-//        try {
-//            jsonBody.put("Username", username);
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-//        String serverAddress = this.getString(R.string.serverAddress);
-//        System.out.println(jsonBody.toString());
-//        String url = serverAddress + "/users/profile";
-//        System.out.println(url);
-//
-//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, jsonBody,
-//                new Response.Listener<JSONObject>() {
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//                        System.out.println(response.toString());
-//
-//                        try {
-//                            String currName = response.getString("Full_Name");
-//                            String currEmail = response.getString("Email");
-//                            String currId = response.getString("License_id");
-//                            String currPhoneNum = response.getString("Phone_number");
-//                            String currBirthday = response.getString("Date_of_birth");
-//                            String[] parts = currBirthday.split("T");
-//                            currBirthday = parts[0];
-//                            String currPassword = response.getString("Password");
-//
-//                            name.setText(currName);
-//                            email.setText(currEmail);
-//                            license.setText(currId);
-//                            phoneNumber.setText(currPhoneNum);
-//                            birthday.setText(currBirthday);
-//                            password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-//                            password.setText(currPassword);
-//
-//
-//
-//
-//
-//
-//
-//
-//                        }
-//                        catch (Exception exception) {
-//
-//                            System.out.println("Error in profile info");
-//                            exception.printStackTrace();
-//                        }
-//                    }
-//                },
-//                new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        // Handle errors
-//                        // You can log the error or show a message to the user
-//
-//                        String text = "Fail"+ error.toString();
-//                        Toast toast = Toast.makeText(getActivity().getApplicationContext(),text, Toast.LENGTH_LONG);
-//                        System.out.println("Fail"+ error.toString());
-//                        toast.show();
-//                    }
-//                });
-//       Volley.newRequestQueue(getActivity().getApplicationContext()).add(jsonObjectRequest);
-//    }
-//
-//
-//
-//    @Override
-//    public void onDestroyView() {
-//        super.onDestroyView();
-//        binding = null;
-//    }
-//}
